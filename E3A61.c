317dbe7cf8584c91846863302dd50055ad2da2f2
Ding <wding280@gmail.com>
2025-04-01 02:43:42 +0800

    加入輸入密碼的程式，原本一直無法清除歡迎畫面的圖案，後來將 clearScreen(); 加入歡迎畫面程式內即可完成。

--- E3A61.c
+++ E3A61.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 
 
+// 清除螢幕
+
+void clearScreen() {
+
+    system("CLS");
+
+}
+
 // 顯示歡迎畫面
 
 void showWelcomeScreen() {
@@ -40,13 +48,55 @@ void showWelcomeScreen() {
     printf("請按ENTER鍵進入下一步\n");
  
     getchar();
+    
+    clearScreen();  // 清除螢幕
+
+}
+
+// 輸入密碼
+
+int checkPassword() 
+{ 
+
+    int password;
+
+    int correctPassword = 8780;
+
+    printf("請輸入4位數密碼: ");
+	 
+    scanf("%d", &password);
+    
+    while (getchar() != '\n');
+
+
+
+    if (password == correctPassword) 
+	{
+
+        printf("密碼正確，恭喜進入系統。\n");
+
+        return 1;
+
+    } 
+	else 
+	{ 
+
+        printf("\a密碼錯誤，麻煩重新輸入。\n");
+
+        return 0;
+
+    }
 
 }
 int main() {
 
 
-
+     
     showWelcomeScreen();
+    
+     // 讓使用者輸入密碼，直到正確為止
+
+    while (!checkPassword());
 
 
 
